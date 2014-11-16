cac46074ae8091253be19b16e075da9bf5f817a1
daniegarcia254 <daniegarcia254@gmail.com>
2014-11-17 00:10:49 +0100

    Version casi final primer sprint

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,74 +1,146 @@
 (function(){
 
-    var app = angular.module('starter.controllers', []);
+    var app = angular.module('starter.controllers',[]);
 
-    var URI = 'http://localhost:8080/com.gps.smartculm/rest/smartculm-service';
+    //var URI = 'http://localhost:8080/com.gps.smartculm/rest/smartculm-service';
+    var URI = 'http://smartculm-danie.rhcloud.com/rest/smartculm-service';
 
     var noticias = [];
+    var incidencias = [];
 
-    app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    app.factory('GetInfoService', function($http, $q, $timeout) {
 
-    });
-
-    app.factory('NoticiasService', function($http, $q, $timeout) {
-
-        var getNoticias = function() {
+        var getNoticias = function () {
 
             var deferred = $q.defer();
-            $timeout( function(){
-                $http.get(URI + '/noticias').then(function(result) {
-                    console.log(result);
-                    //$scope.mydata = result.data;
+            $timeout(function () {
+                $http.get(URI + '/noticias').then(function (result) {
                     deferred.resolve(result.data);
                 });
             });
+            return deferred.promise;
+        };
 
+        var getIncidencias = function () {
+
+            var deferred = $q.defer();
+            $timeout(function () {
+                $http.get(URI + '/incidencias').then(function (result) {
+                    console.log(result.data);
+                    deferred.resolve(result.data);
+                });
+            });
             return deferred.promise;
         };
 
         return {
-            getNoticias : getNoticias
+            getNoticias : getNoticias,
+            getIncidencias : getIncidencias
         };
     });
 
-    app.controller('NoticiasCtrl', function($scope, $http, $ionicLoading, NoticiasService) {
 
-            $scope.currentPage = 0;
-            $scope.pageSize = 6;
+    app.controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicLoading, $rootScope, GetInfoService) {
 
-
+        if (noticias.length == 0) {
             $scope.loadingIndicator = $ionicLoading.show({
-                template: 'Cargando Noticias...',
+                template: 'Cargando Noticias e Incidencias',
                 animation: 'fade-in',
                 showBackdrop: false,
                 maxWidth: 200
             });
-            NoticiasService.getNoticias().then(
-                function(data) {
+
+            GetInfoService.getIncidencias().then(
+                function(data){
                     console.log(data);
+                    incidencias = data;
+                }
+            );
+            GetInfoService.getNoticias().then(
+                function (data) {
+                    console.log(data);
+                    noticias = data;
                     $scope.noticias = data;
+                    $scope.incidencias = incidencias;
                     $ionicLoading.hide();
                 }
             );
+        };
 
+        $scope.reloadInfo = function(){
+            GetInfoService.getIncidencias().then(
+                function(data){
+                    console.log(data);
+                    incidencias = data;
+                    $scope.incidencias = data;
+                }
+            );
+            GetInfoService.getNoticias().then(
+                function (data) {
+                    console.log(data);
+                    noticias = data;
+                    $scope.noticias = data;
+                    $scope.$broadcast('scroll.refreshComplete');
+                }
+            );
+        };
+    });
+
+
+
+    app.controller('NoticiasCtrl', function($scope, $rootScope, $http, $filter, GetInfoService) {
             $scope.numberOfPages=function(){
-                return Math.ceil($scope.noticias.length/$scope.pageSize);
+                return Math.ceil($scope.filteredNoticias.length/$scope.pageSize);
+            };
+
+            $scope.filterNoticias =  function(query) {
+                $scope.filteredNoticias = $filter('filter')($scope.noticias, query);
+                $scope.noOfPages = $scope.numberOfPages();
+            };
+
+            $scope.query = "";
+            $scope.noticias = noticias;
+            $scope.filteredNoticias = noticias;
+            $scope.pageSize = 6;
+            $rootScope.hidePagBtns = false;
+            $scope.currentPage = 0;
+            $scope.noOfPages = $scope.numberOfPages();
+
+
+
+            $scope.reloadNews = function(){
+                if ($scope.currentPage == 0) {
+                    GetInfoService.getNoticias().then(
+                        function (data) {
+                            console.log(data);
+                            $scope.noticias = data;
+                            $scope.filteredNoticias = data;
+                            $rootScope.hidePagBtns = false;
+                            $scope.noOfPages = $scope.numberOfPages();
+                            $scope.query = "";
+                            $scope.$broadcast('scroll.refreshComplete');
+                        }
+                    );
+                };
             };
     });
 
-    app.controller('IncidenciasCtrl', function($scope) {
-        $scope.incidencias = [
-            { title: 'Incidencia 1', id: 1 },
-            { title: 'Incidencia 2', id: 2 },
-            { title: 'Incidencia 3', id: 3 },
-            { title: 'Incidencia 4', id: 4 },
-            { title: 'Incidencia 5', id: 5 },
-            { title: 'Incidencia 6', id: 6 }
-        ];
+
+    app.controller('IncidenciasCtrl', function($scope, $rootScope, $http, $filter, GetInfoService) {
+
+        $scope.incidencias = incidencias;
+
+        $scope.reloadIncidencias = function(){
+            GetInfoService.getIncidencias().then(
+                function (data) {
+                    $scope.incidencias = data;
+                    $scope.$broadcast('scroll.refreshComplete');
+                }
+            );
+        };
     });
 
-    //We already have a limitTo filter built-in to angular,
-    //let's make a startFrom filter
+    //StartFrom filter for the pagination
     app.filter('startFrom', function() {
         return function(input, start) {
             start = +start; //parse to int
